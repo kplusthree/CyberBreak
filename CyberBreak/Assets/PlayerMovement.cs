2615afd47a84190f4fcfdedcf26e100d60ca3956
man1230 <marcusnapoleoni@knights.ucf.edu>
2021-10-08 02:14:32 -0400

    Startup
    Created Player and ground, roughly positioned the camera, and added controller support for movement.

---
+++ CyberBreak/Assets/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 3.0f;
+
+    Rigidbody rigidbody;
+    private float horizontal;
+    private float vertical;
+
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(horizontal, 0, vertical);
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 position = rigidbody.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.z = position.z + speed * vertical * Time.deltaTime;
+
+        rigidbody.MovePosition(position);
+    }
+}
