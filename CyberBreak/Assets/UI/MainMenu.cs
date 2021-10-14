b1805382c7838e60773afae5d8a8165ead4f1f6c
Jessica Wise <jwise1596@knights.ucf.edu>
2021-10-13 20:28:32 -0400

    MenuUpdates

--- CyberBreak/Assets/UI/MainMenu.cs
+++ CyberBreak/Assets/UI/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void LoadGame() 
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("CutScene");
     }
 
     public void QuitGame() 
