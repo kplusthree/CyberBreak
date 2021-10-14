4332a52c72f8c1c577f9828663efbd7e578b3113
MaxwellBustamante <59800680+MaxwellBustamante@users.noreply.github.com>
2021-10-14 11:37:01 -0400

    Tutorial Array for Keyboard controls

---
+++ CyberBreak/Assets/TutorialManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject[] popUps;
+    private int popUpIndex;
+    private float currentWaitTime = -1.0f;
+
+    void Update()
+    {
+        if (currentWaitTime > 0)
+        {
+            currentWaitTime -= Time.deltaTime;
+            return;
+        }
+
+        for (int i = 0; i < popUps.Length; i++)
+        {
+            if(i == popUpIndex)
+            {
+                popUps[i].SetActive(true);
+            }
+            else
+            {
+                popUps[i].SetActive(false);
+            }
+        }
+
+        if (popUpIndex == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+            {
+                currentWaitTime = 2.0f;
+                popUpIndex++;
+            }
+        }
+        else if (popUpIndex == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                currentWaitTime = 2.0f;
+                popUpIndex++;
+            }
+        }
+        else if (popUpIndex == 2)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                currentWaitTime = 2.0f;
+                popUpIndex++;
+            }
+        }
+    }
+}
