828cc126549a42b582308b4b1db0e01e8bda59ff
man1230 <marcusnapoleoni@knights.ucf.edu>
2021-10-28 17:31:20 -0400

    expanded gameController function

--- CyberBreak/Assets/Scripts/gameController.cs
+++ CyberBreak/Assets/Scripts/gameController.cs
@@ -7,28 +7,50 @@ public class gameController : MonoBehaviour
     private bool endLevel;
 
     public PlayerController player;
+    private int currentPlayerLives;
+    private int previousLivesAmount;
+
     public BossController boss;
+    private int currentBossHealth;
+    private int previousHealthAmount;
+
     // Start is called before the first frame update
     void Start()
     {
+        currentPlayerLives = player.lives;
+        previousLivesAmount = currentPlayerLives;
+        currentBossHealth = boss.health;
+        previousHealthAmount = currentBossHealth;
         endLevel = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        currentPlayerLives = player.lives;
+        currentBossHealth = boss.health;
+
+        if (currentBossHealth != previousHealthAmount)
+        {
+            Debug.Log(currentBossHealth);                   // Update boss health ui here
+            previousHealthAmount = currentBossHealth;
+        }
+        if (currentPlayerLives != previousLivesAmount)
+        {
+            Debug.Log(currentPlayerLives);                  // Update player health ui here
+            previousLivesAmount = currentPlayerLives;
+        }
+
         if(endLevel == false)
         {
             if (boss.gameWin == true)
             {
-                // Add win state here
-                Debug.Log("you win!");
+                Debug.Log("you win!");                      // Add win state here
                 endLevel = true;
             }
             else if (player.gameOver == true)
             {
-                //Add loose state here
-                Debug.Log("you lose!");
+                Debug.Log("you lose!");                     //Add loose state here
                 endLevel = true;
             }
         }
