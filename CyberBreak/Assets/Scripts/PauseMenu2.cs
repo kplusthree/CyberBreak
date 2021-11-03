add8ef2ef505e7f96d091e26c1f89edc29c6f205
Jessica Wise <jwise1596@knights.ucf.edu>
2021-11-03 07:04:47 -0400

    Updated UI
    still trying to fix pause menu

---
+++ CyberBreak/Assets/Scripts/PauseMenu2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu2 : MonoBehaviour
+{
+      public GameObject pauseMenuUI;
+
+      public static bool GameIsPaused = false;
+
+      void Update()
+      {
+          if(Gamepad.current.aButton.wasPressedThisFrame)
+          { 
+            if (GameIsPaused)
+            {
+              ResumeGame();
+            }
+            else
+            {
+              PauseGame();
+            }
+          }
+      }
+
+      public void PauseGame()
+      {
+          Time.timeScale = 0f;
+          pauseMenuUI.SetActive(true);
+          GameIsPaused = true;
+      }
+
+      public void ResumeGame()
+      {
+        Time.timeScale = 1;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+      }
+
+         public void ReturnMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
