cc7445bbb41cd6f67747606a9890a53fac61e617
Kortney Menefee <kplusmenefee@gmail.com>
2021-11-20 14:18:59 -0500

    Hallway Set Up

---
+++ CyberBreak/Assets/Scripts/hallwayGameController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class hallwayGameController : MonoBehaviour
+{
+    private bool endLevel;
+    [HideInInspector]
+    public PlayerController player;
+    private int currentPlayerLives;
+    private int previousLivesAmount;
+
+    private bool setMusic;
+
+    public AudioClip hallwayMusicClip;
+    public AudioSource musicSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        currentPlayerLives = player.lives;
+        previousLivesAmount = currentPlayerLives;
+        endLevel = false;
+        setMusic = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+
+        currentPlayerLives = player.lives;
+
+        if (setMusic == false)
+        {
+
+            musicSource.clip = hallwayMusicClip;
+
+            musicSource.Play();
+
+            setMusic = true;
+        }
+
+        if (currentPlayerLives != previousLivesAmount)
+        {
+            Debug.Log(currentPlayerLives);                  // added to player script
+            previousLivesAmount = currentPlayerLives;
+        }
+
+        /*
+        set to actual win conditions
+        if (endLevel == false)
+        {
+            if (boss.gameWin == true && scene.name == "Courtyard")
+            {
+                Debug.Log("you win!");
+                endLevel = true;
+                SceneManager.LoadScene("InvestigatorCutscene");
+            }
+        }
+
+        */
+    }
+}
