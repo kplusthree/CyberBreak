32bfd7b11781a088fb63b8775a8f1074b45a21e5
Kortney Menefee <59934488+kplusthree@users.noreply.github.com>
2021-11-22 14:51:16 -0500

    Clown Fix

---
+++ CyberBreak/Assets/Scripts/SideScrollerEnemyController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideScrollerEnemyController : MonoBehaviour
+{
+    [HideInInspector]
+    public int health;
+    [HideInInspector]
+    public int maxHealth = 10;
+    public bool gameWin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+        gameWin = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            gameWin = true;
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "PlayerBullet")
+        {
+            //health = health - 5;
+            TakeDamage(10);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+}
